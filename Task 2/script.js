863166f10527dcc5f961c78d1566f613af0165f9
Konstancija Fomenko <konstancija.fomenko@codeacademylt.onmicrosoft.com>
2023-04-28 23:07:51 +0300

    task2

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,15 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+var counter = 0;
+const outputDiv = document.getElementById('btn__state');
+
+document
+  .getElementById('btn__element')
+  .addEventListener('click', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    counter += 1;
+    outputDiv.innerHTML = counter;
+  });
