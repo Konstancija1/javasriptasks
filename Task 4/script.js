47edf6f9e3d4302c131cea34af52984c3bc46079
Konstancija Fomenko <konstancija.fomenko@codeacademylt.onmicrosoft.com>
2023-04-30 20:17:59 +0300

    task4

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,43 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+fetch('cars.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const carsByManufacturer = groupBy(data, 'brand');
+
+    const cards = Object.keys(carsByManufacturer).map((manufacturer) => {
+      const models = carsByManufacturer[manufacturer]
+        .map((car) => car.model)
+        .join(', ');
+
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      const title = document.createElement('h2');
+      title.textContent = manufacturer;
+      card.appendChild(title);
+
+      const modelList = document.createElement('ul');
+      models.split(',').forEach((model) => {
+        const modelItem = document.createElement('li');
+        modelItem.textContent = model.trim();
+        modelList.appendChild(modelItem);
+      });
+
+      card.appendChild(modelList);
+
+      return card;
+    });
+
+    const outputDiv = document.getElementById('output');
+    cards.forEach((card) => outputDiv.appendChild(card));
+  })
+  .catch((error) => console.error(error));
+
+function groupBy(list, key) {
+  return list.reduce((groups, item) => {
+    const group = groups[item[key]] || [];
+    return { ...groups, [item[key]]: [...group, item] };
+  }, {});
+}
