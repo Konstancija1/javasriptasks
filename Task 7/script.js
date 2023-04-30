76020fb3db6d49abb54b0b383722c152e78a050d
Konstancija Fomenko <konstancija.fomenko@codeacademylt.onmicrosoft.com>
2023-04-30 22:32:09 +0300

    task7

--- Task 7/script.js
+++ Task 7/script.js
@@ -4,9 +4,15 @@ Turimas "audi" objektas.
 Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
+
 const audi = {
   make: 'audi',
   model: 'A6',
   year: 2005,
   color: 'white',
 };
+
+function showObjectKeys(obj) {
+  return Object.keys(obj);
+}
+const keys = showObjectKeys(audi);
