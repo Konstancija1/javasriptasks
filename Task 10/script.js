b2af624a06d546218f275ffe51d49903dd70f52b
Konstancija Fomenko <konstancija.fomenko@codeacademylt.onmicrosoft.com>
2023-05-02 00:53:19 +0300

    task9

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,6 +2,17 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+const composition = require('./composition');
+const division = require('./division');
+const multiplication = require('./multiplication');
+const substraction = require('./substraction');
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,16 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+const movie1 = new Movie('Batman', 'Joker', 260000000);
+const movie2 = new Movie('Avatar', 'Tom Cruise', 55000000);
+const movie3 = new Movie('Marilyn Monroe', 'Brad Pitt', 58000000);
